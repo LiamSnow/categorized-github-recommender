604e64eb133e03000e4b551493e56f8b9368007a
LiamSnow <mail@liamsnow.com>
2024-11-07 15:47:37 -0500

    added pagination

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -16,51 +18,66 @@ func readAPIKey() string {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	 return os.Getenv("GITHUB_API_KEY")
+	return os.Getenv("GITHUB_API_KEY")
 }
 
-func main() {
-  api_key := readAPIKey()
+func getRepos(client *http.Client, api_key string, since int) ([]Repository, error) {
+	baseURL := "https://api.github.com/repositories"
 
-	url := "https://api.github.com/repositories"
-	req, err := http.NewRequest("GET", url, nil)
+	u, err := url.Parse(baseURL)
 	if err != nil {
-		fmt.Printf("Error making request: %v\n", err)
-		return
+		return nil, fmt.Errorf("Error parsing URL: %v", err)
+	}
+
+	q := u.Query()
+	q.Add("since", strconv.Itoa(since))
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating request: %v", err)
 	}
 
 	req.Header.Add("Accept", "application/vnd.github+json")
 	req.Header.Add("Authorization", "Bearer "+api_key)
 	req.Header.Add("X-GitHub-Api-Version", "2022-11-28")
 
-	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Error executing request: %v\n", err)
+		return nil, fmt.Errorf("Error executing request: %v", err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Printf("Status: %s\n", resp.Status)
-
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Error reading response body: %v\n", err)
-		return
+		return nil, fmt.Errorf("Error reading response body: %v", err)
 	}
 
 	var repositories []Repository
 	err = json.Unmarshal(body, &repositories)
 	if err != nil {
-		fmt.Printf("Error unmarshaling JSON: %v\n", err)
-		return
+		return nil, fmt.Errorf("Error unmarshaling JSON: %v", err)
 	}
 
-	fmt.Printf("Number of repositories: %d\n", len(repositories))
+	return repositories, nil
+}
 
-	if len(repositories) > 0 {
-		fmt.Printf("First repository: %d by %s\n",
-			repositories[0].ID,
-			repositories[0].FullName,
-		)
-	}
+func main() {
+	api_key := readAPIKey()
+	client := &http.Client{}
+
+  since := 0
+  num_found := 0
+
+  for i := 0; i < 100; i++ {
+    repos, err := getRepos(client, api_key, since)
+    if err != nil {
+      fmt.Printf("Error getting repos: %v\n", err)
+      return
+    }
+
+    num_found += len(repos)
+    fmt.Printf("%d\n", num_found)
+    since = repos[len(repos)-1].ID
+  }
 }
